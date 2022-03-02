0b4c103ff51884824220410bce6c3aa291edf33b
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-02 16:01:22 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+namespace alistirma_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiler = new int[10];
+            Random random = new Random();
+            int tek = 0, cift =0;
+            for (int i=0; i<10; i++)
+            {
+                sayiler[i] = random.Next();
+                int a = sayiler[i]%2 == 1 ? tek++ : cift++;
+                Console.WriteLine(sayiler[i]);
+            }
+            Console.WriteLine("cift sayilerin sayisi "+ cift);
+            Console.WriteLine("tek sayilerin saiyis "+ tek);
+        }
+    }
+}
